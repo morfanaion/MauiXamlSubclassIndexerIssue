8b8af7ef62f5c662717cc27da1b2c17a4ce0205e
morfanaion <163520469+morfanaion@users.noreply.github.com>
2024-04-17 13:51:27 +0200

    Delete BusinessLibrary directory

--- BusinessLibrary/Models/RealModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BusinessLibrary.Models
-{
-    public class RealModel : SubModel
-    {
-        public string Text3 => "And this works as expected";
-    }
-}
--- BusinessLibrary/Models/SubModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BusinessLibrary.Models
-{
-    public class SubModel : BaseModel
-    {
-        public string Text2 => "This however works just fine";
-        public override string Text4 => "SubModelText";
-    }
-}
